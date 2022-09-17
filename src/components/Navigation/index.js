036c91f9c0ce0b5faebb9a749b2b42ae63786401
angiebunk1 <103800228+angiebunk1@users.noreply.github.com>
2022-09-17 18:46:25 -0500

    fixed nav highlight

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -9,7 +9,7 @@ function Navigation (props) {
         <nav>
             <ul className="nav">
                 {sections.map((Section) => (
-                    <li sectionName={`${currentSection.name === Section.name && 'highlight'}`} key={Section.name}>
+                    <li className={`${currentSection.name === Section.name && 'active'}`} key={Section.name}>
                         <span onClick={() => setCurrentSection(Section)}>{Section.name}</span>
                     </li>
                 ))}
